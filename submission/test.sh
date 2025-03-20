9753c76e7d77799b4510c2605e9cd0da97538060
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-20 23:29:55 +0100

    fix: week-one

--- submission/test.sh
+++ submission/test.sh
@@ -173,11 +173,15 @@ echo "Create a descriptor for your taproot address and derive the address to ens
 NEW_TAPROOT_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32m")
 check_cmd "New taproot address generation"
 NEW_TAPROOT_ADDR=$(trim "$NEW_TAPROOT_ADDR")
+echo "New taproot address: $NEW_TAPROOT_ADDR"
 
 # STUDENT TASK: Get the address info to extract the internal key
 # WRITE YOUR SOLUTION BELOW:
-ADDR_INFO= $(bitcoin-cli -regtest getaddressinfo $NEW_TAPROOT_ADDR)
+ADDR_INFO= $(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $NEW_TAPROOT_ADDR)
 check_cmd "Getting address info"
+
+# Debugging: Print the address info for reference
+echo "Address info: $ADDR_INFO"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
 # WRITE YOUR SOLUTION BELOW:
