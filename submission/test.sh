a7880962743658e3ec1555556d07fa6f5df1f778
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-20 21:51:31 +0100

    fix: week-one-done

--- submission/test.sh
+++ submission/test.sh
@@ -170,13 +170,13 @@ echo "Create a descriptor for your taproot address and derive the address to ens
 
 # STUDENT TASK: Create a new taproot address
 # WRITE YOUR SOLUTION BELOW:
-NEW_TAPROOT_ADDR= $(bitcoin-cli -regtest getnewaddress "" "bech32m")
+NEW_TAPROOT_ADDR= $(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32m")
 check_cmd "New taproot address generation"
 NEW_TAPROOT_ADDR=$(trim "$NEW_TAPROOT_ADDR")
 
 # STUDENT TASK: Get the address info to extract the internal key
 # WRITE YOUR SOLUTION BELOW:
-ADDR_INFO= $(bitcoin-cli -regtest getaddressinfo $NEW_TAPROOT_ADDR)
+ADDR_INFO= $(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $NEW_TAPROOT_ADDR)
 check_cmd "Getting address info"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
@@ -193,14 +193,14 @@ echo "Simple descriptor: $SIMPLE_DESCRIPTOR"
 
 # STUDENT TASK: Get a proper descriptor with checksum
 # WRITE YOUR SOLUTION BELOW:
-TAPROOT_DESCRIPTOR= $(bitcoin-cli -regtest getdescriptorinfo "$SIMPLE_DESCRIPTOR")
+TAPROOT_DESCRIPTOR= $(bitcoin-cli -regtest -rpcwallet=btrustwallet getdescriptorinfo "$SIMPLE_DESCRIPTOR")
 check_cmd "Descriptor generation"
 TAPROOT_DESCRIPTOR=$(trim "$TAPROOT_DESCRIPTOR")
 echo "Taproot treasure map: $TAPROOT_DESCRIPTOR"
 
 # STUDENT TASK: Derive an address from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-DERIVED_ADDR_RAW= $(bitcoin-cli -regtest deriveaddresses "$TAPROOT_DESCRIPTOR" 0)
+DERIVED_ADDR_RAW= $(bitcoin-cli -regtest -rpcwallet=btrustwallet deriveaddresses "$TAPROOT_DESCRIPTOR" 0)
 check_cmd "Address derivation"
 DERIVED_ADDR=$(echo "$DERIVED_ADDR_RAW" | tr -d '[]" \n\t')
 echo "Derived quantum vault address: $DERIVED_ADDR"
